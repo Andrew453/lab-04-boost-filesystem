a004f12535feff55138d04eb2cb4159bde8ac5a7
ProkiProk <senior.prockuschev2017@yandex.ru>
2021-11-17 22:17:43 +0300

    v1.2

--- demo/main.cpp
+++ demo/main.cpp
@@ -6,13 +6,4 @@ int main() {
   DirectoryWalker directoryWalker;
   directoryWalker.read_directory("../misc/ftp", false);
   directoryWalker.output_statistics();
-//  string a = "balance_01234567_76543210.txt";
-//  cmatch m;
-//  std::string object = "/home/prokiprok/CLionProjects/labs/lab-04-boost-filesystem/misc/ftp/bcs/balance_01234567_76543210.txt";
-//  regex_search(object.c_str(), m, regex("([a-z]+)/balance_([0-9]+)_([0-9]+).txt"));
-////    regex_search("/home/prokiprok/CLionProjects/labs/lab-04-boost-filesystem/misc/ftp/bcs/balance_01234567_76543210.txt", m, regex("([a-z]+)/balance_([0-9]+)_([0-9]+).txt"));
-//    cout << m[0] << endl;
-//    cout << m[1] << endl;
-//    cout << m[2] << endl;
-//    cout << m[3] << endl;
 }
